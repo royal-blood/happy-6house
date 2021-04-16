3bed284fdc01081f0db8011bdd266ccc77068b02
hoon <hoonti06@gmail.com>
2021-04-16 15:26:40 +0900

    Spring boot 프로젝트 생성

---
+++ src/main/java/com/royalblood/happy6house/Happy6houseApplication.java
@@ -0,0 +1,13 @@
+package com.royalblood.happy6house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Happy6houseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Happy6houseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/royalblood/happy6house/Happy6houseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royalblood.happy6house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Happy6houseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
