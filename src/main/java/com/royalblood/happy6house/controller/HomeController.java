9251852060050624410b90f2032019d583f5cd4c
hoon <hoonti06@gmail.com>
2021-04-24 14:53:34 +0900

    fix : ouath2 제거로 인한 UI 오류 수정

--- src/main/java/com/royalblood/happy6house/controller/HomeController.java
+++ src/main/java/com/royalblood/happy6house/controller/HomeController.java
@@ -1,18 +1,12 @@
 package com.royalblood.happy6house.controller;
 
-import com.royalblood.happy6house.config.auth.LoginUser;
-import com.royalblood.happy6house.config.auth.dto.SessionUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(Model model, @LoginUser SessionUser user) {
-        if(user != null){
-            model.addAttribute("userName", user.getName());
-        }
+    public String home() {
         return "home";
     }
 }
